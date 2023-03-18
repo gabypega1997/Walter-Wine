1776d487ef49cbda5b7b990f9b8d874ebf717d56
gabypega1997 <gabypega1997@gmail.com>
2023-03-18 11:28:25 +0100

    create the basic navbar

--- common/components/navbar/index.tsx
+++ common/components/navbar/index.tsx
@@ -1,5 +1,19 @@
+import NavbarButton from "./navbar-button";
+
 const Navbar = () => {
-    return <div> Navbar</div>;
+    return (
+        <>
+            <NavbarButton link="/">Home</NavbarButton>
+            <NavbarButton link="/about">About us</NavbarButton>
+            <NavbarButton link="/cart" style="shop">
+                Shopping Cart
+            </NavbarButton>
+            <NavbarButton link="/shop" style="shop">
+                Shop
+            </NavbarButton>
+            <NavbarButton link="/auth">Authentication</NavbarButton>
+        </>
+    );
 };
 
 export default Navbar;
--- common/components/navbar/navbar-button.tsx
+++ common/components/navbar/navbar-button.tsx
@@ -1,10 +1,22 @@
-import Link from "next/link"
+import { FC, ReactNode } from "react";
+import Link from "next/link";
 
+type NavbarButtonProps = {
+    children: ReactNode;
+    link: string;
+    style?: string;
+};
 
-type NavbarButton
+const NavbarButton: FC<NavbarButtonProps> = ({ children, link, style }) => {
+    return (
+        <Link
+            href={link}
+            className={
+                `${style ? "text-red-500" : "text-yellow-500"}`}
+        >
+            {children}
+        </Link>
+    );
+};
 
-const NavbarButton = ({children, link, style}) => {
-    return (<Link>
-    {children}
-    </Link>)
-}
+export default NavbarButton;
