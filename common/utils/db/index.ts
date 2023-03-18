bfcadb4f718c664d9318e3dffc20be0ae837933a
gabypega1997 <gabypega1997@gmail.com>
2023-03-18 14:55:57 +0100

    add firebase connections setup 10%

---
+++ common/utils/db/index.ts
@@ -0,0 +1,14 @@
+import { ServiceAccount } from "firebase-admin";
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
+
+if (!admin.apps.length) {
+    try {
+        admin.initializeApp({
+            credential: admin.credential.cert(<ServiceAccount>serviceAccount),
+        });
+    } catch (error) {
+        console.log("Firebase admin initialization error", error.stack);
+    }
+}
+export default admin.firestore();
