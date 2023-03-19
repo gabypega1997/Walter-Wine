7bcc8649d4a458e4cd7f5b9fea08e5a166286564
gabypega1997 <gabypega1997@gmail.com>
2023-03-19 20:49:35 +0100

    create a get api for firebase

--- pages/api/wines/index.ts
+++ pages/api/wines/index.ts
@@ -1,21 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 import db from "@/common/utils/db";
 
-export default async (res, req) => {
-    try {
-        const { slug } = req.body;
-        const entries = await db.collection("entries").get();
-        const entriesData = entries.docs((entry) => entry.data());
-
-        if (entriesData.some((entry) => entry.slug === slug)) {
-            res.status(400).end();
-        } else {
-            const { id } = await db.collection("entries").add({
-                ...req.body,
-                created: new Date().toISOString(),
-            });
-            res.status(200).json({ id });
-        }
-    } catch (error) {
-        res.status(400).end();
-    }
+type Data = {
+    id: string;
+    name: string;
 };
+
+export default async function winesHandler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data[] | { message: string }>
+) {
+    if (req.method === "GET") {
+        try {
+            const winesRef = await db.collection("wines").get();
+            const wines: Data[] = [];
+
+            winesRef.forEach((wine) => {
+                wines.push({
+                    id: wine.id,
+                    name: wine.data().name,
+                });
+            });
+            res.status(200).json(wines);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Server error" });
+        }
+    } else {
+        res.status(404).json({ message: "Not found" });
+    }
+}
