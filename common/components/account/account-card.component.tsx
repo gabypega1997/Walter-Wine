403266043f6bd4ac51321f2be7306d682b658505
gabypega1997 <gabypega1997@gmail.com>
2023-04-09 11:06:24 +0200

    make links for account options

--- common/components/account/account-card.component.tsx
+++ common/components/account/account-card.component.tsx
@@ -1,21 +1,39 @@
 import { selectUser } from "@/common/store/user/user.selector";
-import Image from "next/image";
 import { useSelector } from "react-redux";
+import SignOut from "../sign-out";
+import NavigationButtonAccount from "./navigation-button-account.component";
 import { ProfilImage } from "./profil-image.component";
-import { UploadProfilImage } from "./upload-profil-image.component";
 
 const AccountCard = () => {
     const user = useSelector(selectUser);
-    console.log(user);
-
 
     return (
         <div className="flex flex-col justify-center items-center gap-5">
             <h1 className="text-2xl font-medium">My Account</h1>
-            <ProfilImage user={user}/>
-            {user && user.displayName}
-            <br />
-            {user && user.email}
+            {user && <ProfilImage user={user} />}
+
+            <NavigationButtonAccount
+                link="/account/my-orders"
+                imageUrl="/defaultProfil.jpg"
+            >
+                My Orders
+            </NavigationButtonAccount>
+
+            <NavigationButtonAccount
+                link="/account/my-cart"
+                imageUrl="/defaultProfil.jpg"
+            >
+                My Cart
+            </NavigationButtonAccount>
+
+            <NavigationButtonAccount
+                link="/account/account-settings"
+                imageUrl="/defaultProfil.jpg"
+            >
+                Account Settings
+            </NavigationButtonAccount>
+
+            <SignOut />
         </div>
     );
 };
