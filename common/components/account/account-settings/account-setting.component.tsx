f2bae4760edc3ad5c79f021becff4106141ba02a
gabypega1997 <gabypega1997@gmail.com>
2023-04-12 21:09:29 +0200

    to send a better json string to update user api

--- common/components/account/account-settings/account-setting.component.tsx
+++ common/components/account/account-settings/account-setting.component.tsx
@@ -2,17 +2,19 @@ import { selectUser } from "@/common/store/user/user.selector";
 import { useSelector } from "react-redux";
 import SignOut from "../../sign-out";
 import { ProfilImage } from "../profil-image.component";
+import UpdateInput from "./update-input.component";
+import { UploadProfilImage } from "./upload-profil-image.component";
 const AccountSettings = () => {
     const user = useSelector(selectUser);
     return (
         <div>
             <h1>AccountSetting</h1>
             <ProfilImage user={user} />
+            <UploadProfilImage user={user} />
+            <UpdateInput user={user} type={"name"} />
+            <UpdateInput user={user} type={"email"} />
 
-        <label htmlFor="name">Name</label>
-        <input value={user.displayName}/>
-
-            <SignOut/>
+            {/* <SignOut/> */}
         </div>
     );
 };
