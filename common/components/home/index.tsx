3e2178aee64580d850dc2f509f343c9bae5a0660
gabypega1997 <gabypega1997@gmail.com>
2023-04-27 20:08:18 +0200

    create a review but without blur

--- common/components/home/index.tsx
+++ common/components/home/index.tsx
@@ -11,10 +11,23 @@ const Home = () => {
                 alt="bottle of wine"
                 className="mx-auto pt-10"
             />
-            <h2 className="text-white text-3xl font-bold">
+            <h2 className="text-white text-3xl font-bold w-[330px] mx-auto my-8 ">
                 Welcome to the world of wines, where taste and aroma blend with
                 art and passion.
             </h2>
+            <div className="flex">
+                <div className="bg-white">
+                    &quot;Wines vary in taste, aroma, origin, technique.&quot;
+                    -Jack
+                </div>
+                <div className=" bg-white">
+                    <div className="w-full h-full flex flex-col justify-center items-center backdrop-blur-lg">
+                        {" "}
+                        More Reviews
+                    </div>
+                    More Reviews
+                </div>
+            </div>
             <Button type="buy">Buy Now</Button>
             <Button type="read">Read More</Button>
         </div>
