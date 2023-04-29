ed7a13cc6e5dabf7613d108925982c566ebcd8be
gabypega1997 <gabypega1997@gmail.com>
2023-04-29 20:30:49 +0200

    fix the navbar

--- common/components/home/index.tsx
+++ common/components/home/index.tsx
@@ -3,20 +3,20 @@ import Button from "../button/button.component";
 
 const Home = () => {
     return (
-        <div className="bg-gray-dark h-screen ">
+        <div className="bg-gray-dark py-10 ">
             <Image
                 src="/images/home/bottleWine.png"
                 width={300}
                 height={200}
                 alt="bottle of wine"
-                className="mx-auto pt-10"
+                className="mx-auto"
             />
             <h2 className="text-white text-3xl font-bold w-[330px] mx-auto my-8 ">
                 Welcome to the world of wines, where taste and aroma blend with
                 art and passion.
             </h2>
             <div className="flex justify-between items-center text-lg gap-2">
-                <div className="bg-white bg-opacity-40 py-1  rounded-2xl px-8 leading-5">
+                <div className="bg-white bg-opacity-40 py-3  rounded-2xl px-8 leading-5">
                     &quot;Wines vary in taste, aroma, origin, technique.&quot;
                     <br />
                     <span className="ml-28">-Jack</span>
