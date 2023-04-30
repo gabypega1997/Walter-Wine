d9c9554a05f533c8b69f90130ea9f5a76b3d7d1f
gabypega1997 <gabypega1997@gmail.com>
2023-04-30 09:03:57 +0200

    add ap poligon background to my jumbotron

--- common/components/home/index.tsx
+++ common/components/home/index.tsx
@@ -3,22 +3,26 @@ import Button from "../button/button.component";
 
 const Home = () => {
     return (
-        <div className="bg-gray-dark py-10 ">
+        <div className=" pb-10 bg-gray-dark ">
+            {/* Background poligons */}
+
             {/* jumbotron */}
-            <Image
-                src="/images/home/bottleWine.png"
-                width={300}
-                height={200}
-                alt="bottle of wine"
-                className="mx-auto"
-            />
-            <h2 className="text-white text-3xl font-bold w-[330px] mx-auto my-8 ">
-                Welcome to the world of wines, where taste and aroma blend with
-                art and passion.
-            </h2>
+            <div className="bg-[url('/images/home/Polygon1.png')]  bg-[left_bottom_5rem] bg-no-repeat pt-10">
+                <Image
+                    src="/images/home/bottleWine.png"
+                    width={300}
+                    height={200}
+                    alt="bottle of wine"
+                    className="mx-auto"
+                />
+                <h2 className="text-white text-3xl font-bold w-[330px] mx-auto my-8 ">
+                    Welcome to the world of wines, where taste and aroma blend
+                    with art and passion.
+                </h2>
+            </div>
 
             {/* review  */}
-            <div className="flex justify-between items-center text-lg gap-2">
+            <div className="flex justify-between items-center text-lg gap-2 bg-[url('/images/home/Polygon2.png')]">
                 <div className=" py-3  rounded-2xl px-8 leading-5 backdrop-blur-lg bg-white/30">
                     <p className="backdrop-shadow-lg">
                         &quot;Wines vary in taste, aroma, origin,
@@ -32,9 +36,9 @@ const Home = () => {
                     More Reviews
                 </div>
             </div>
-            {/* Background poligons */}
 
-            <div className="flex flex-col gap-5 mt-12 items-end mr-12">
+            {/* Buttons */}
+            <div className="flex flex-col gap-5 mt-12 items-end mr-12 bg-[url('/images/home/Polygon3.png')]">
                 <Button type="buy">Buy Now</Button>
                 <Button type="read">Read More</Button>
             </div>
