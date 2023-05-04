2647bbd8f3d3a5976e2c92b2ca253b810a92231f
gabypega1997 <gabypega1997@gmail.com>
2023-05-04 19:30:13 +0200

    finished about page

--- common/components/about/contact.component.tsx
+++ common/components/about/contact.component.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+
 const Contact = () => {
-    return <div>
-        <h3 className="text-brown font-bold text-center">Contact us</h3>
+    return (
         <div>
-            
+            <h3 className="text-brown font-bold text-center">Contact us</h3>
+            <div className="flex justify-center gap-12 pt-6 pb-12">
+                <Image
+                    width={35}
+                    height={35}
+                    alt="phone"
+                    src="/images/about/phone.png"
+                ></Image>
+                <Image
+                    width={35}
+                    height={35}
+                    alt="email"
+                    src="/images/about/email.png"
+                ></Image>
+            </div>
         </div>
-    </div>;
+    );
 };
 
 export default Contact;
