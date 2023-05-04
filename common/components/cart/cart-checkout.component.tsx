7fa654673e17bf65ba3a439dd63f8b33b0589fbe
gabypega1997 <gabypega1997@gmail.com>
2023-05-04 20:16:54 +0200

    create a price component

--- common/components/cart/cart-checkout.component.tsx
+++ common/components/cart/cart-checkout.component.tsx
@@ -8,6 +8,7 @@ import { CartItem, Wine } from "@/common/types/wine.types";
 import Image from "next/image";
 import { selectUser } from "@/common/store/user/user.selector";
 import Payment from "../payment-form";
+import Link from "next/link";
 
 const CartCheckout = () => {
     const cartItems = useSelector(selectCartItems);
@@ -18,15 +19,20 @@ const CartCheckout = () => {
         <div>
             {/* Images component */}
 
-            <h1 className="p-3 text-2xl font-bold text-center text-white bg-slate-400">
+            <h1 className="p-3 text-2xl font-bold text-center text-white bg-gray-dark">
                 Checkout
             </h1>
-            <div className="rounded-br-[250px] h-96 bg-slate-400 flex max-w-full overflow-scroll">
+            <div className="rounded-br-[250px] h-96 bg-gray-dark flex max-w-full overflow-scroll drop-shadow-2xl">
+                {!cartItems.length && (
+                    <div className="text-white text-xl p-5">
+                        Your cart is empty
+                    </div>
+                )}
                 {cartItems &&
                     cartItems.map((item: Wine) => (
                         <div
                             key={item.id}
-                            className="flex p-5 m-5 overflow-scroll bg-yellow-200 h-52 rounded-3xl min-w-fit"
+                            className="flex p-5 m-5 overflow-scroll bg-yellow h-52 rounded-3xl min-w-fit"
                         >
                             <Image
                                 src="/wine1.png"
@@ -39,27 +45,41 @@ const CartCheckout = () => {
             </div>
 
             {/*   Price compoentn   */}
-            <div>
+            <div className="p-4">
                 {cartItems &&
                     cartItems.map((item: CartItem) => (
-                        <div key={item.id}>
-                            <div>
-                                {item.title}
-                                {": x"}
-                                {item.quantity}
-                                {" $"}
-                                {item.price}{" "}
+                        <div key={item.id} className="font-semibold">
+                            <div className="flex py-1">
+                                <div className="w-3/5">
+                                    {item.title}
+                                    {":"}
+                                </div>
+                                <div className="w-1/5">
+                                    {"x"}
+                                    {item.quantity}
+                                </div>
+                                <div className="w-1/5">
+                                    {" $"}
+                                    {item.price}{" "}
+                                </div>
                             </div>
                         </div>
                     ))}
-                <p>Total: {cartTotal}$</p>
+
+                {cartTotal > 0 && <p>Total: {cartTotal}$</p>}
             </div>
 
             {/* LogIn Component */}
             {currentUser ? (
                 <Payment />
             ) : (
-                <div>Please LogIn to continue with your order</div>
+                <div className="text-lg p-16">
+                    You need to be{" "}
+                    <Link href="auth" className="text-wine font-bold">
+                        registred
+                    </Link>{" "}
+                    to use Payments
+                </div>
             )}
         </div>
     );
