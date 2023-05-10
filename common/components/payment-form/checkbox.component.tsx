f9cdf5c0f7d0290c7af7c212973d44c337bff548
gabypega1997 <gabypega1997@gmail.com>
2023-05-10 19:42:06 +0200

    create a checkbox

--- common/components/payment-form/checkbox.component.tsx
+++ common/components/payment-form/checkbox.component.tsx
@@ -8,15 +8,21 @@ type CheckboxProps = {
 const Checkbox: FC<CheckboxProps> = ({ children, checkboxFor }) => {
     return (
         <div className="flex gap-3 py-2 text-lg text-brown font-semibold items-center relative">
-            <input
-                type="checkbox"
-                id={checkboxFor}
-                name={checkboxFor}
-                className="ring-4 ring-brown w-5 h-5 accent-brown appearance-none  checked:bg-brown checked:border-brown
+            <div className="ring-4 ring-brown w-5 h-5">
+                <input
+                    type="checkbox"
+                    id={checkboxFor}
+                    name={checkboxFor}
+                    className=" w-5 h-5 accent-brown appearance-none  checked:bg-brown checked:border-brown
+                
             "
-            style={{  transformOrigin: "bottom left",
-                clipPath: "polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%)"}}
-            />
+                    style={{
+                        transformOrigin: "bottom left",
+                        clipPath:
+                            "polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%)",
+                    }}
+                />
+            </div>
 
             <label htmlFor={checkboxFor}>{children}</label>
         </div>
