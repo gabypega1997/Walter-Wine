b288db6073a0532bf30a78bb7054ab194f2525a6
gabypega1997 <gabypega1997@gmail.com>
2023-05-13 09:21:56 +0200

    create a layout for auth page

---
+++ common/components/auth/index.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "./auth-layout.component";
+
+const Authentication = () => {
+    return (
+        <div className="min-h-screen bg-gray-dark py-16">
+            <AuthLayout />
+        </div>
+    );
+};
+
+export default Authentication;
