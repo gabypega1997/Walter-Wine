fa44737f4b0a581e447035ad4cae47ae6ad77830
gabypega1997 <gabypega1997@gmail.com>
2023-05-13 19:32:35 +0200

    create a logic for state to auth

---
+++ common/components/auth/auth-template.component.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import SignUpForm from "../sign-up-form";
+import SignInForm from "../sign-in-form";
+
+const AuthTemplate = () => {
+    const [signUpOrSignIn, setSignUpOrSignIn] = useState<"signIn" | "signUp">(
+        "signIn"
+    );
+
+    return (
+        <div className="">
+            {signUpOrSignIn === "signIn" ? (
+                <div className="text-center my-10 text-white">
+                    <h2 className="text-xl font-bold">
+                        I alredy have an account{" "}
+                    </h2>
+                    <p className="opacity-70 italic">
+                        <span className="text-brown">Sign in</span> with your
+                        email and<br></br> password
+                    </p>
+                    <SignInForm />
+                    <div
+                        className="text-xl font-semibold text-yellow"
+                        onClick={() => setSignUpOrSignIn("signUp")}
+                    >
+                        Don&apos;t have an acoount ?
+                    </div>
+                </div>
+            ) : (
+                <div className="text-center my-10 text-white">
+                    <h2 className="text-xl font-bold">
+                        Don&apos;t have an acoount ?{" "}
+                    </h2>
+                    <p className="opacity-70 italic">
+                        <span className="text-yellow">Sign up</span> with your
+                        email <br />
+                        and password
+                    </p>
+                    <SignUpForm />
+                    <div
+                        className="text-xl font-semibold text-yellow"
+                        onClick={() => setSignUpOrSignIn("signIn")}
+                    >
+                        You alredy have an account ?
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AuthTemplate;
