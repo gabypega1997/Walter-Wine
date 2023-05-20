51566df2fef3b71e32196ac4feabe341b1b5eef2
gabypega1997 <gabypega1997@gmail.com>
2023-05-20 10:07:22 +0200

    create a blur for confirmation

--- common/components/confirmation/confirmation-message.component.tsx
+++ common/components/confirmation/confirmation-message.component.tsx
@@ -1,5 +1,9 @@
 const ConfirmationMessage = () => {
-    return <>Your order is on its way</>;
+    return (
+        <div className="h-[400px] rounded-br-[200px] bg-[url('/images/confirmation/frankfurt.png')] flex justify-center items-center">
+            <div className="h-28 w-full bg-white/30 flex justify-center items-center text-white text-2xl font-bold backdrop-blur-sm ">Your order is on its way</div>
+        </div>
+    );
 };
 
 export default ConfirmationMessage;
