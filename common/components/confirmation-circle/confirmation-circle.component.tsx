4d5c306b10f73d5dcba7d4b2a5e97ea5ce31d398
gabypega1997 <gabypega1997@gmail.com>
2023-05-21 10:55:43 +0200

    create a svg for a check sign

--- common/components/confirmation-circle/confirmation-circle.component.tsx
+++ common/components/confirmation-circle/confirmation-circle.component.tsx
@@ -1,9 +1,23 @@
-const ConfirmationCircle = () => {
-    return (<div className="w-10 h-10 border-4 border-green-600 rounded-full " >
-        <div className="w-4 h-1.5 bg-green-600 rounded-full"></div>
-        <div className="w-8 h-1.5 bg-green-600 rounded-full"></div>
-        <div></div>
-        </div>)
+const ConfirmationCircle = ({ ...otherProps }) => {
+    return (
+        <div {...otherProps}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="green"
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="rounded-full w-80 h-80"
+            >
+                <path d="M20 6L9 17l-5-5"></path>
+            </svg>
+        </div>
+    );
 };
-
+// style={{
+//     clipPath:
+//     "polygon(25% 50%, 40% 65%, 75% 30%, 70% 25%, 40% 55%, 30% 45%)",
+// }}
 export default ConfirmationCircle;
--- common/components/confirmation/truck-animation.component.tsx
+++ common/components/confirmation/truck-animation.component.tsx
@@ -1,10 +1,12 @@
 import ConfirmationCircle from "../confirmation-circle/confirmation-circle.component";
 
 const TruckAnimation = () => {
-    return <>Truck Animation 
-    <ConfirmationCircle/>
-    </>;
+    return (
+        <>
+            Truck Animation
+            <ConfirmationCircle className="flex items-center justify-center w-20 h-20 border-8 border-green-700 rounded-full" />
+        </>
+    );
 };
-
 
 export default TruckAnimation;
