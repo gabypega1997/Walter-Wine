e6b4ce0e701ee416a248232dd164bca005e12e4f
gabypega1997 <gabypega1997@gmail.com>
2023-05-25 20:01:37 +0200

    create a animation for confirmation

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,41 @@ module.exports = {
                 yellow: "#F4E6B0",
                 brown: "#CC8E6E",
             },
+            keyframes: {
+                truck: {
+                    "0%": { transform: "translateX(0)" },
+
+                    "40%": { transform: "translateX(calc(60vw))" },
+
+                    "50%": { transform: " translateX(calc(60vw))" },
+
+                    "60%": {
+                        transform: "translateX(calc(58vw)) rotate(-6deg)",
+                    },
+
+                    "100%": { transform: "translateX(140vw) rotate(-6deg)" },
+                },
+                arrow: {
+                    "0%": { transform: "translateX(0)" },
+
+                    "40%": { transform: "translateX(calc(60vw))" },
+
+                    "50%": { transform: " translateX(calc(60vw))" },
+
+                    "60%": { transform: "translateX(calc(58vw)) " },
+
+                    "100%": { transform: "translateX(140vw) " },
+                },
+                confirmation: {
+                    "0%": { transform: "translateX(0)" },
+                    "100%": { transform: "translateX(calc(50vw + 80px))" },
+                },
+            },
+            animation: {
+                truck: "truck 4s ease",
+                arrow: "arrow 4s ease",
+                confirmation: "confirmation 1s ease-out 4s forwards ",
+            },
         },
     },
     plugins: [],
