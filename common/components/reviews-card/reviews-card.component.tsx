6756d91af31bbe54dd68ca7f1f38561b8df86a93
gabypega1997 <gabypega1997@gmail.com>
2023-05-29 09:51:47 +0200

    create a reviews page

--- common/components/reviews-card/reviews-card.component.tsx
+++ common/components/reviews-card/reviews-card.component.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import StarsRating from "../products/stars-rating.component";
+import { FC } from "react";
 
-const ReviewsCard = () => {
+type ReviewsCardProps = {
+    image: string;
+    name: string;
+    message: string;
+    ratting: number;
+};
+
+const ReviewsCard: FC<ReviewsCardProps> = ({
+    image,
+    name,
+    message,
+    ratting,
+}) => {
     return (
         <div className="bg-gray-dark pt-28 pb-3">
             <div className="bg-gray-light mx-5 flex rounded-2xl shadow-white/40 shadow-md">
                 <div className="w-2/5">
                     <Image
-                        src="/images/reviews/Jonathan.png"
+                        src={`/images/reviews/${image}.png`}
                         width={150}
                         height={200}
                         alt="Jonathan"
@@ -15,12 +28,12 @@ const ReviewsCard = () => {
                     />
                 </div>
                 <div className="w-3/5 text-white text-lg flex flex-col justify-between items-center py-2">
-                    <h3 className="text-2xl">Jonathan Lynch</h3>
-                    <p className="mr-auto ml-6"> Great</p>
+                    <h3 className="text-2xl">{name}</h3>
+                    <p className="mr-auto ml-6 leading-5 py-1">{message}</p>
                     <div className="flex gap-3">
-                        <StarsRating rating={5} />
+                        <StarsRating rating={ratting} />
                         <span className="font-sans font-semibold -mt-1">
-                            5.0
+                            {ratting.toFixed(1)}
                         </span>
                     </div>
                 </div>
--- pages/reviews/index.tsx
+++ pages/reviews/index.tsx
@@ -1,10 +1,53 @@
 import ReviewsCard from "@/common/components/reviews-card/reviews-card.component";
 
+type reviewsArrayShape = {
+    name: string;
+    image: string;
+    message: string;
+    ratting: number;
+};
+
+const reviewsArray: reviewsArrayShape[] = [
+    {
+        name: "Jonathan Lynch",
+        image: "Jonathan",
+        message: "Greate wine",
+        ratting: 4.8,
+    },
+    {
+        name: "Brandon Stanley",
+        image: "Brandon",
+        message: "The best wine ever",
+        ratting: 5.0,
+    },
+    {
+        name: "Ruth Brewer",
+        image: "Ruth",
+        message: "The best wine for partys",
+        ratting: 4.0,
+    },
+    {
+        name: "Fionna Lucas",
+        image: "Fionna",
+        message: "I enjoy every evening a glas and is like a bless",
+        ratting: 4.9,
+    },
+];
+
 const Reviews = () => {
     return (
-        <div>
-            <ReviewsCard />
-            <ReviewsCard />
+        <div className="min-h-screen bg-gray-dark">
+            {reviewsArray.map(({ name, image, message, ratting }, index) => {
+                return (
+                    <ReviewsCard
+                        key={index}
+                        name={name}
+                        image={image}
+                        message={message}
+                        ratting={ratting}
+                    />
+                );
+            })}
         </div>
     );
 };
